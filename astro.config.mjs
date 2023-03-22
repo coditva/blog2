ab6276272efe03b2df41faaa57f7ae2afab252a5
Utkarsh Maheshwari <coditva@gmail.com>
2023-03-22 22:32:41 +0530

    Use vercel_url for site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://coditva.dev",
+  site: "https://" + (process.env.VERCEL_URL || "coditva.dev"),
   integrations: [
     tailwind(),
     image({
